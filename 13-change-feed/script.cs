aaae9cb0492098259f10bbcebefb90637eb0a1be
Georgios Hadjiharalambous <40407855+giorgosHadji@users.noreply.github.com>
2024-12-07 13:51:33 +0000

    Update script.cs
    add the full implementation of the changeFeed processing

--- 13-change-feed/script.cs
+++ 13-change-feed/script.cs
@@ -1,7 +1,39 @@
-using Microsoft.Azure.Cosmos;
-using static Microsoft.Azure.Cosmos.Container;
+ using Microsoft.Azure.Cosmos;
+ using static Microsoft.Azure.Cosmos.Container;
 
-string endpoint = "<cosmos-endpoint>";
-string key = "<cosmos-key>";
+ string endpoint = "<cosmos-endpoint>";
+ string key = "<cosmos-key>";
 
-CosmosClient client = new CosmosClient(endpoint, key);
+ CosmosClient client = new CosmosClient(endpoint, key);
+    
+ Container sourceContainer = client.GetContainer("cosmicworks", "products");
+ Container leaseContainer = client.GetContainer("cosmicworks", "productslease");
+    
+ ChangesHandler<Product> handleChanges = async (
+     IReadOnlyCollection<Product> changes,
+     CancellationToken cancellationToken
+ ) => {
+     Console.WriteLine($"START\tHandling batch of changes...");
+     foreach(Product product in changes)
+     {
+         await Console.Out.WriteLineAsync($"Detected Operation:\t[{product.id}]\t{product.name}");
+     }
+ };
+    
+ var builder = sourceContainer.GetChangeFeedProcessorBuilder<Product>(
+         processorName: "productsProcessor",
+         onChangesDelegate: handleChanges
+     );
+    
+ ChangeFeedProcessor processor = builder
+     .WithInstanceName("consoleApp")
+     .WithLeaseContainer(leaseContainer)
+     .Build();
+    
+ await processor.StartAsync();
+    
+ Console.WriteLine($"RUN\tListening for changes...");
+ Console.WriteLine("Press any key to stop");
+ Console.ReadKey();
+    
+ await processor.StopAsync();
